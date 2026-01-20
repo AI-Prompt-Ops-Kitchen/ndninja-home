67d2e9c92ede973600a178acc4a3bfa82c0514b1
Neurodivergent Ninja <neurodivergent.ninja@users.noreply.github.com>
2026-01-20 16:45:54 -0600

    chore: add KeywordDetector and DetectionResult to tools module exports

--- tools_db/tools/__init__.py
+++ tools_db/tools/__init__.py
@@ -2,9 +2,12 @@
 
 from .automation_hub import AutomationHub
 from .production_checker import ProductionChecker, CheckResult
+from .keyword_detector import KeywordDetector, DetectionResult
 
 __all__ = [
     "AutomationHub",
     "ProductionChecker",
     "CheckResult",
+    "KeywordDetector",
+    "DetectionResult",
 ]
